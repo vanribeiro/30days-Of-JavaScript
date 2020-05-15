7480086c28b4ed86ee1c5e78c7952e99f142fca9
Van Ribeiro <vanribeiro.dev@gmail.com>
2020-05-15 02:47:56 -0300

    done day 12.

---
+++ challenge-files/12 - Key Sequence Detection/README.md
@@ -0,0 +1,33 @@
+# Day 12 - Key Sequence Detection ✅
+
+**Date:** 05/14/2020
+
+![Array Cardio](./../../images/challenges/12-key-sequence-detection.png)
+
+## About HTML and CSS
+
+Some tags to create new features!
+
+## About JavaScript
+
+I learned about cool things here but I didn't have time to write here again. Sorry... 
+
+I'll try to do it on the next days! 😊
+
+## About my modifications
+
+Adapting the Wes code, I tried to do a kind of key words detection.
+
+## Conclusion
+
+This activity is was really funny! 😊💖
+
+You can see final result [here](https://vanribeiro-30daysofjavascript.netlify.app/challenge-files/12%20-%20key%20sequence%20detection/)). 😃😉😍
+
+That's all folks! 😃
+
+Thanks [WesBos](https://github.com/wesbos) to share this with us! 😊💖
+
+---
+
+written by [@vanribeiro](https://github.com/vanribeiro).
---
+++ challenge-files/12 - Key Sequence Detection/js/main.js
@@ -0,0 +1,33 @@
+const input = document.querySelector('#secret-word');
+const rightAnswerSound = document.querySelector('#right-answer-sound');
+const secretWord = document.querySelector('.strong');
+const errorMessage = document.querySelector('.error-answer');
+input.value;
+
+let secretWords = ['banana', 'javascript', 'cupcake', 'blue', 'love'];
+let randomWord = secretWords[getRandowWords(secretWords)];
+
+function getRandowWords(secretWords){
+  let min = 0, max = secretWords.length;
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+const pressed = [];
+const secretCode = randomWord;
+secretWord.innerHTML = `${randomWord}`;
+input.addEventListener('keyup', (e) =>{
+  if(e.keyCode > 64 && e.keyCode < 91){
+    pressed.push(e.key);
+    pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+    if(pressed.join('').includes(secretCode)){
+      rightAnswerSound.play();
+      secretWord.style.color = 'green';
+      input.style.color ='green';
+      errorMessage.textContent = '';
+    } else{
+      input.style.color = 'red';
+      errorMessage.textContent = 'If you type any letter wrong, please refresh the page to get a new secret word!';
+    }
+  } 
+
+})
