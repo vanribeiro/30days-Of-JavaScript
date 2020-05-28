7fc8492a1da3eea945d80be3d6fc8a67ce60ef6c
Vanessa Ribeiro <vanribeiro.dev@gmail.com>
2020-05-28 01:53:51 -0300

    update readme and comments.

--- README.md
+++ README.md
@@ -28,7 +28,7 @@ Nº | Day        | Description            | Status
 18|05/22/2020 |[Adding Up Times with Reduce](challenge-files/18%20-%20Adding%20Up%20Times%20with%20Reduce/) | ✅
 19|05/25/2020 |[Webcam Fun](challenge-files/19%20-%20Webcam%20Fun/) | ✅
 20|05/26/2020 |[Speech Detection](challenge-files/20%20-%20Speech%20Detection/) | ✅
-
+21|05/27/2020 |[Geolocation](challenge-files/20%20-%20Geolocation/) | ✅
 ___
 
 
--- challenge-files/21 - Geolocation/js/main.js
+++ challenge-files/21 - Geolocation/js/main.js
@@ -4,11 +4,10 @@ const speed = document.querySelector('.speed-value');
 navigator.geolocation.watchPosition((data) => {
     /**
      * data.coords.speed returns meters/seconds
-     * 3.6km/h = 1m/s
-     * 
+     * 1m/s = 3.6km/h
      */
     speed.textContent = (data.coords.speed * 3.6).toFixed(5);
-    arrow.style.transform = `rotate(${data.coords.heading}deg)`
+    arrow.style.transform = `rotate(${data.coords.heading}deg)`;
 }, (err) => {
     console.err(err);
     alert('HEY! YOU GOTTA ALLOW THAT TO HAPPEN!!!');
