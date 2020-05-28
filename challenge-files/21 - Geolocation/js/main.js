a998c958fb46693abf2cec7157f48677d71f8b1b
Vanessa Ribeiro <vanribeiro.dev@gmail.com>
2020-05-28 00:52:38 -0300

    fix speed inner HTML.

--- challenge-files/21 - Geolocation/js/main.js
+++ challenge-files/21 - Geolocation/js/main.js
@@ -2,7 +2,7 @@ const arrow = document.querySelector('.arrow');
 const speed = document.querySelector('.speed-value');
 
 navigator.geolocation.watchPosition((data) => {
-    // speed.textContent = data.coords.speed;
+    speed.textContent = data.coords.speed;
     arrow.style.transform = `rotate(${data.coords.heading}deg)`
 }, (err) => {
     console.err(err);
